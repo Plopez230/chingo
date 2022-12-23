e67435d3b7cbe25c331c984294c2ba76c565d804
Plopez230 <plopez230@gmail.com>
2022-12-23 19:38:28 +0100

    Add views and urls

---
+++ chingo/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'chingo'
+
+urlpatterns = [
+	path('list/<int:list_id>/', views.list_view, name='list'),
+	path('list/<int:list_id>/edit/', views.list_edit_view, name='list_edit'),
+	path('list/<int:list_id>/edit_word/', views.word_edit_view, name='word_edit'),
+	path('list/<int:list_id>/add/<int:word_id>/', views.word_add_from_database_view, name='word_add_from_db'),
+	path('list/<int:list_id>/remove/<int:word_id>/', views.word_remove_view, name='word_remove'),
+	path('list/<int:list_id>/add/', views.word_add_view, name='word_add'),
+	path('list/add/', views.list_add_view, name='list_add'),
+	path('search/', views.search_view, name='search'),
+	path('test/', views.test_view, name='test'),
+	path('check_test/', views.test_check_view, name='test_check'),
+	path('', views.index_view, name='index'),
+]
--- chingo/views.py
+++ chingo/views.py
@@ -1,3 +1,104 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from django.urls import reverse
+from django.shortcuts import redirect, get_object_or_404
+from .models import *
+from .forms import *
+from . import chingo_game as game
 
 # Create your views here.
+def index_view(request):
+    template = loader.get_template('chingo/index.html')
+    context = {
+        'lists': WordList.objects.all(),
+        'user': request.user,
+        }
+    return HttpResponse(template.render(context, request))
+
+def search_view(request):
+    template = loader.get_template('chingo/search.html')
+    keyword = request.GET.get("keyword")
+    context = {
+        'lists': game.search_lists(keyword),
+        'words': game.search_words(keyword),
+        'user': request.user,
+        }
+    return HttpResponse(template.render(context, request))
+
+def list_view(request, list_id):
+    template = loader.get_template('chingo/list.html')
+    current_list = get_object_or_404(WordList, id=list_id)
+    context = {
+        'list': current_list,
+        'parts_of_speech': Word.PartOfSpeech.choices,
+        'words': Word.objects.all()
+    }
+    return HttpResponse(template.render(context, request))
+
+def test_view(request):
+    template = loader.get_template('chingo/test.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
+
+def test_check_view(request):
+    template = loader.get_template('chingo/check_test.html')
+    context = {"wrong": True}
+    return HttpResponse(template.render(context, request))
+
+def word_add_from_database_view(request, list_id, word_id):
+    if request.method == "POST":
+        word = get_object_or_404(Word, id=word_id)
+        list = get_object_or_404(WordList, id=list_id)
+        list.words.add(word)
+    response = redirect(reverse('chingo:list', kwargs={'list_id':list_id}))
+    return response
+
+def word_add_view(request, list_id):
+    word_list = get_object_or_404(WordList, pk=list_id)
+    if request.method == "POST":
+        form = WordForm(request.POST)
+        if form.is_valid():
+            new_word = form.save()
+            word_list.words.add(new_word)
+            game.score_word(request.user)
+    response = redirect(reverse('chingo:list', kwargs={'list_id':list_id}))
+    return response
+
+def list_add_view(request):
+    if request.method == "POST":
+        form = WordListForm(request.POST)
+        if form.is_valid():
+            new_list = form.save()
+            game.score_list(request.user, new_list)
+    response = redirect(reverse('chingo:list', kwargs={'list_id':new_list.id}))
+    return response
+
+def word_remove_view(request, list_id, word_id):
+    if request.method == "POST":
+        word = get_object_or_404(Word, id=word_id)
+        list = get_object_or_404(WordList, id=list_id)
+        if list.words.contains(word):
+            list.words.remove(word)
+    response = redirect(reverse('chingo:list', kwargs={'list_id':list_id}))
+    return response
+
+def list_edit_view(request, list_id):
+    word_list = get_object_or_404(WordList, pk=list_id)
+    if request.method == "POST":
+        form = WordListForm(request.POST, instance=word_list)
+        if form.is_valid():
+            new_word = form.save()
+    response = redirect(reverse('chingo:list', kwargs={'list_id':list_id}))
+    return response
+
+def word_edit_view(request, list_id):
+    if request.method == "POST":
+        word_list = get_object_or_404(WordList, pk=list_id)
+        word_id = request.POST.get("word_id")
+        word = get_object_or_404(Word, pk=word_id)
+        form = WordForm(request.POST, instance=word)
+        if form.is_valid():
+            form.save()
+    response = redirect(reverse('chingo:list', kwargs={'list_id':list_id}))
+    return response
