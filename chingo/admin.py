a3ec8c6c20c4e5e80646a3906cb16624790911af
Plopez230 <plopez230@gmail.com>
2022-12-23 23:56:23 +0100

    Admin: Register models

--- chingo/admin.py
+++ chingo/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.register(ChingoUser)
+admin.register(Word)
+admin.register(WordList)
+admin.register(Score)
