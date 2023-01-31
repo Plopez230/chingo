c194c10d37cd15222b974d7c46f98d7c7dcdccb4
Plopez230 <plopez230@gmail.com>
2023-01-31 04:28:33 +0100

    Speech synth 2

--- chingo/utils.py
+++ chingo/utils.py
@@ -1,5 +1,7 @@
-
+from django.conf import settings
 import math
+import pyttsx3
+import os
 
 def split_columns(lst):
     chunk1 = math.ceil(len(lst)/3)
@@ -10,3 +12,19 @@ def split_columns(lst):
         lst[chunk1:chunk2],
         lst[chunk2:],
         ]
+
+def sing(word):
+    espeak_path = os.path.join(settings.BASE_DIR, 'espeak')
+    if not os.path.exists(espeak_path):
+        os.makedirs(espeak_path)
+    filename = os.path.join(espeak_path, str(word.id)+'_espeak.mp3')
+    mandarin = pyttsx3.init()
+    mandarin.setProperty('voice', 'zh')
+    mandarin.setProperty('rate', 130)
+    mandarin.save_to_file(word.pinyin, filename)
+    mandarin.runAndWait()
+    mandarin.stop()
+    del(mandarin)
+    while not os.path.exists(filename) or os.path.getsize(filename) == 0:
+        pass
+    return filename
