61df272c885e5577fa5c35e2c6be6501837759b3
ruzafa8 <34567477+ruzafa8@users.noreply.github.com>
2023-02-06 14:36:11 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 python manage.py collectstatic --noinput
 python manage.py makemigrations
 python manage.py migrate
-/opt/bitnami/ctlscript.sh restart apache
+sudo /opt/bitnami/ctlscript.sh restart apache
